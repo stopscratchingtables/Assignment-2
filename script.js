70453b7e22497d04a516932e71c6b60dd9e0203e
stopscratchingtables <hugovonlouwen@gmail.com>
2020-12-30 01:10:37 +0800

    okk

--- script.js
+++ script.js
@@ -52,10 +52,43 @@ function displayLakers() {
         document.getElementById("off_score").innerHTML = org;
     })
 
+    let active_players_id_list = []
+    let active_players_name_list = []
+    // Team indiv stats
+    fetch("https://api-nba-v1.p.rapidapi.com/players/teamId/17", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "7ef7505eeemsh4b7ae28b990ec32p10a9e5jsnf404942f045e",
+            "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        const a = response.api
+        console.log(a.players);
+        const b = a.players
+        for (let i=0; i < b.length; i++)
+        {
+            if (b[i].yearsPro > 1)
+            {
+                const c=b[i];
+                active_players_id_list.push(c.playerId);
+                const name = c.firstName + " " + c.lastName;
+                active_players_name_list.push(name);
+            }
+        }    
+        for (var x=0; x<15 ;x++)
+        {
+            var p_id = active_players_id_list[x];
+            var p_name = active_players_name_list[x];
+            buildTable(p_id, p_name);
+        }
+    })
+    .catch(err => {
+        console.error(err);
+    });
 };
-
-
-
 
 
 
@@ -134,75 +167,78 @@ function displayNets() {
                 active_players_name_list.push(name);
             }
         }
-        console.log(active_players_name_list[0]);
-        document.getElementById("p_name").innerHTML = active_players_name_list[0];
-
-        //  Player ID and their statistics
-        var ppg = 0;
-        var apg = 0;
-        var rpg = 0;
-        var pm = 0;
-        for(x<)
-        fetch("https://api-nba-v1.p.rapidapi.com/statistics/players/playerId/" + active_players_id_list[x], {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "7ef7505eeemsh4b7ae28b990ec32p10a9e5jsnf404942f045e",
-                "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
-            }
-        })
-        .then(response => response.json())
-        .then(response => {
-            console.log(response);
-            const a = response.api
-            const b = a.statistics
-            console.log(b);
-            for(let i=0; i<82; i++)
-            {
-                if(b[i].points > 0 )
-                {
-                    ppg += parseFloat(b[i].points);
-                }
-
-                if (b[i].assists > 0)
-                {
-                    apg += parseFloat(b[i].assists);
-                }
-
-                if (b[i].rebounds > 0)
-                {
-                    rpg += parseFloat(b[i].rebounds);
-                }
-
-                if (b[i].plusMinus > 0)
-                {
-                    pm += parseFloat(b[i].plusMinus);
-                }
-            }
-            ppg = (ppg/82);
-            apg = (apg/82);
-            rpg = (rpg/82);
-            pm = (pm/82);
-            console.log(ppg, apg, rpg, pm);
-            /*
-            document.getElementById("ppg").innerHTML = ppg.toFixed(2);
-            document.getElementById("apg").innerHTML = apg.toFixed(2);
-            document.getElementById("rpg").innerHTML = rpg.toFixed(2);
-            document.getElementById("pm").innerHTML = pm.toFixed(2);*/
-        })/*
-        .catch(err => {
-            console.error(err);
-        });*/
+        
+        for (var x=0; x<15 ;x++)
+        {
+            var p_id = active_players_id_list[x];
+            var p_name = active_players_name_list[x];
+            buildTable(p_id, p_name);
+        }
 
     })
     .catch(err => {
         console.error(err);
     });
-
-    
 }
 
+function buildTable(a, b){
+    //  Player ID and their statistics
+    var ppg = 0;
+    var apg = 0;
+    var rpg = 0;
+    var pm = 0;
+    fetch("https://api-nba-v1.p.rapidapi.com/statistics/players/playerId/" + a, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "7ef7505eeemsh4b7ae28b990ec32p10a9e5jsnf404942f045e",
+            "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        const x = response.api
+        const y = x.statistics
+        for(let i=0; i<82; i++)
+        {
+            if (y.length >= 82)
+                if(y[i].points > 0 )
+                {
+                        ppg += parseFloat(y[i].points);
+                }
 
+                if (y[i].assists > 0)
+                {
+                        apg += parseFloat(y[i].assists);
+                }
 
+                if (y[i].totReb > 0)
+                {
+                        rpg += parseFloat(y[i].totReb);
+                }
+
+                if (y[i].plusMinus > 0)
+                {
+                        pm += parseFloat(y[i].plusMinus);
+                }
+            }
+    ppg = (ppg/82);
+    apg = (apg/82);
+    rpg = (rpg/82);
+    pm = (pm/82)
+
+    document.getElementById('team_ranking').innerHTML += `<tr>
+                                                            <th scope="row">${b}</th>
+                                                            <td>${ppg.toFixed(2)}</td>
+                                                            <td>${apg.toFixed(2)}</td>
+                                                            <td>${rpg.toFixed(2)}</td>
+                                                            <td>${pm.toFixed(2)}</td>
+                                                        </tr>`
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
 
 
 
@@ -253,11 +289,48 @@ function displayNuggets() {
         let org = 0
         org += total_hts * 0.9;
         document.getElementById("off_score").innerHTML = org.toFixed(2);
+
+
+        let active_players_id_list = []
+        let active_players_name_list = []
+        // TEam indiv stats
+        fetch("https://api-nba-v1.p.rapidapi.com/players/teamId/9", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "7ef7505eeemsh4b7ae28b990ec32p10a9e5jsnf404942f045e",
+                "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+            }
+        })
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            const a = response.api
+            console.log(a.players);
+            const b = a.players
+            for (let i=0; i < b.length; i++)
+            {
+                if (b[i].yearsPro > 1)
+                {
+                    const c=b[i];
+                    active_players_id_list.push(c.playerId);
+                    const name = c.firstName + " " + c.lastName;
+                    active_players_name_list.push(name);
+                }
+            }
+            
+            for (var x=0; x<15 ;x++)
+            {
+                var p_id = active_players_id_list[x];
+                var p_name = active_players_name_list[x];
+                buildTable(p_id, p_name);
+            }
+    
+        })
+        .catch(err => {
+            console.error(err);
+        });
     })
 }
-
-
-
 
 
 
@@ -310,5 +383,45 @@ function displayHeat() {
         let org = 0
         org += total_hts * 0.9;
         document.getElementById("off_score").innerHTML = org.toFixed(2);
+
+
+        let active_players_id_list = []
+        let active_players_name_list = []
+        // TEam indiv stats
+        fetch("https://api-nba-v1.p.rapidapi.com/players/teamId/20", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "7ef7505eeemsh4b7ae28b990ec32p10a9e5jsnf404942f045e",
+                "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+            }
+        })
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            const a = response.api
+            console.log(a.players);
+            const b = a.players
+            for (let i=0; i < b.length; i++)
+            {
+                if (b[i].yearsPro > 1)
+                {
+                    const c=b[i];
+                    active_players_id_list.push(c.playerId);
+                    const name = c.firstName + " " + c.lastName;
+                    active_players_name_list.push(name);
+                }
+            }
+            
+            for (var x=0; x<15 ;x++)
+            {
+                var p_id = active_players_id_list[x];
+                var p_name = active_players_name_list[x];
+                buildTable(p_id, p_name);
+            }
+    
+        })
+        .catch(err => {
+            console.error(err);
+        });
     })
 }
