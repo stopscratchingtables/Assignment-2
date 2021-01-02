eeca4870a48129cbbdabc21cee8b32b86a6976a6
stopscratchingtables <hugovonlouwen@gmail.com>
2021-01-02 13:24:27 +0800

    new

--- script.js
+++ script.js
@@ -4,14 +4,47 @@
 // =======================================
 // =======================================
 
-function darkMode() {
-    let darkMode = localStorage.getItem('darkMode')
+let darkMode = localStorage.getItem('darkMode')
+const darkModeToggle = document.querySelector('#darkModebutton')
 
+// check if dark mode enabled
+// if enabled, turn off
+// if disabled, turn on
+
+const enableDarkMode =  () => {
+    
+    document.nav.classList.add('')
+    //document.body.classList.add('darkmode');
+    localStorage.setItem('darkMode', "enabled");
 
 }
 
+const disableDarkMode = () => {
 
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkMode", null)
 
+}
+
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+function darkMode() {
+
+    darkMode = localStorage.getItem('darkMode');
+
+    if (darkMode !== 'enabled')
+    {
+        enableDarkMode();
+        console.log(darkMode);
+    }
+    else 
+    {
+        disableDarkMode();
+    }
+
+}
 
 
 
